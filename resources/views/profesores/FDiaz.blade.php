1227ad02629101e667bce05fe5f0d76385fe92b5
WinEd73 <edwinyankov2000@gmail.com>
2022-12-07 15:11:40 -0600

    FDiazProf Agregado

--- resources/views/profesores/FDiaz.blade.php
+++ resources/views/profesores/FDiaz.blade.php
@@ -45,7 +45,7 @@
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
                         aria-expanded="true" aria-controls="collapseOne">
-                        <strong>Actividad académica</strong>
+                        <strong>Actividad académica</strong> 
                     </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
