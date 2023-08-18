9b6bf7d279982c85ff08fe2f03792322b71297e1
AgendaAmb <eugenia.almendarez@uaslp.mx>
2023-08-18 13:06:07 -0500

    Calendario 2023-2024

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -3,7 +3,7 @@
 @section('maincontent')
 
 <div class="col-12 col-sm-12 p-5 img-fluid">
-    <img src="{{asset('storage/images/Calendario_2022-2023.jpg')}}" alt="">
+    <img src="{{asset('storage/images/Calendario2023-2024.jpg')}}" alt="">
 </div>
 
 @endsection
